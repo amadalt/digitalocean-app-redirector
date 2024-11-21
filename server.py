576813617648532d05cc7da94ebcd3dfe43a1b05
amadalt <ahmadaltamimi79@outlook.com>
2024-11-21 19:51:10 +1100

    server.py

--- server.py
+++ server.py
@@ -1,52 +1,13 @@
 #!/usr/bin/python3
-#
-# This is a boilerplate Flask reverse-proxy application
-# to be deployed in DigitalOcean Apps as a Web App.
-#
-# Usage:
-#    1. Go to Digital Ocean's App Platform:
-#       https://cloud.digitalocean.com/apps
-#
-#    2. Click "Create App" and then "Link your Github Account"
-#
-#    3. Choose "All Repositories" or select only this one
-#
-#    4. Click "Install and Authorize"
-#
-#    5. Select this repository
-#
-#    6. Pick repo's branch
-#
-#    7. Change Run Command to:
-#         waitress-serve --port 443 --call server:create_app
-#
-#    8. Change port to 443
-# 
-#    9. Click Next -> select Basic App -> click "Launch App"
-# 
-#   10. Wait to retrieve URL.
-#
-# Mariusz Banach / mgeeky, '22-'23
-# <mb [at] binary-offensive.com>
-#
 
 import os
 import flask
 import requests
 import urllib.parse
 
-############################################################
-# CONFIGURATION
+teamserver          = os.environ.get('TEAMSERVER', '185.208.158.155')
 
-# Point onto your Teamserver or a landing page you want to reverse-proxy.
-# Cannot contain http:// , https:// and trailing slash
-
-teamserver          = os.environ.get('TEAMSERVER', 'www.bbc.com')
-
-# If true, will set Host header with value from `teamserver`
 rewrite_host_header = True
-
-############################################################
 
 method_requests_mapping = {
     'GET': requests.get,
